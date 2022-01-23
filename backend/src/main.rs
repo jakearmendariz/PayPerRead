079af39f6faffd86a3ea51285906ad74066160ab
Jake Armendariz <jakearmendariz99@gmail.com>
2022-01-23 14:14:49 -0800

    cors changes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,6 +6,9 @@ extern crate dotenv_codegen;
 mod mongo;
 mod common;
 mod reader;
+use rocket_cors::{AllowedHeaders, AllowedOrigins};
+use rocket::http::Method;
+use rocket::{get, routes};
 
 #[get("/")]
 fn index() -> String {
@@ -13,10 +16,23 @@ fn index() -> String {
 }
 
 fn main() {
+    let allowed_origins = AllowedOrigins::some_exact(&["http://localhost:5000/"]);
+
+    // You can also deserialize this
+    let cors = rocket_cors::CorsOptions {
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors().expect("CORS failure");
+
     let mongo_db = mongo::connect_to_mongo()
         .expect("Could not connect to mongoDB, try adding your IP address to allowlist.");
     rocket::ignite()
         .manage(mongo_db)
+        .attach(cors)
         .mount(
             "/",
             routes![
