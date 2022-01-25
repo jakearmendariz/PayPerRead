909bc81e261df2b640148092b0e1515e0319fc6d
Xin <xin2001yan@gmail.com>
2022-01-25 00:41:00 -0800

    added router for navigating between user and publisher pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import App from './App';
+import UserLandingPage from './pages/UserLandingPage'
+import PublisherLandingPage from './pages/PublisherLandingPage'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="user" element={<UserLandingPage />} />
+        <Route path="publisher" element={<PublisherLandingPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/pages/PublisherLandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function PublisherLandingPage() {
+
+    return (
+      <div style={{padding: "30px"}}>
+        FOR PUBLISHERS
+      </div>
+    );
+}
+
+export default PublisherLandingPage;
