a18cedac34c2dcec2faa8adeab4ea5096c2d5eb9
Xin <xin2001yan@gmail.com>
2022-01-30 16:22:47 -0800

    refactor unit test

--- backend/tests/publishers.py
+++ backend/tests/publishers.py
@@ -3,16 +3,38 @@ from pickle import ADDITEMS
 import requests
 import unittest
 
-from backend.tests.readers import TEST_USER, TEST_USER_WITH_MONEY
-
 ADDRESS = "http://localhost:8000"
-TEST_EMAIL = "test2@gmail.com"
+TEST_EMAIL = "test@gmail.com"
 TEST_DOMAIN = 'www.example.com'
 NO_MONEY = {'dollars': 0, 'cents': 0}
 MONEY = {'dollars': 1000, 'cents': 55}
-NEW_USER = {'email': TEST_EMAIL, 'name': 'Test User', 'domain': TEST_DOMAIN}
-TEST_USER = {'email': TEST_EMAIL, 'name': 'Test User', 'domain': TEST_DOMAIN}
-TEST_USER_WITH_MONEY = {'email': TEST_EMAIL, 'name': 'Test User', 'balance': MONEY}
+TEST_USER_NAME = 'Test User'
+NEW_USER = {'email': TEST_EMAIL, 'name': TEST_USER_NAME, 'domain': TEST_DOMAIN}
+TEST_USER = {'email': TEST_EMAIL, 'name': TEST_USER_NAME, 'domain': TEST_DOMAIN}
+TEST_USER_WITH_MONEY = {'email': TEST_EMAIL, 'name': TEST_USER_NAME, 'balance': MONEY}
+
+def get_publishers():
+    return requests.get(f"{ADDRESS}/publishers")
+    
+def add_publisher():
+    return requests.post(f"{ADDRESS}/publisher/new-publisher", json=NEW_USER)
+
+def get_publisher():
+    return requests.get(f"{ADDRESS}/publisher/{TEST_EMAIL}")
 
 
+class PublisherTests(unittest.TestCase):
 
+    def test_publisher_does_not_exist(self):
+        get_publisher_not_found = get_publisher()
+        self.assertEqual(get_publisher_not_found.status_code, 404)
+
+    def test_insert_then_delete_publisher(self):
+        add_publisher_result = add_publisher()
+        self.assertEqual(add_publisher_result.status_code, 201)
+        get_publisher_result = get_publisher()
+        self.assertEqual(get_publisher_result.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- backend/tests/readers.py
+++ backend/tests/readers.py
@@ -5,8 +5,11 @@ Expensive, don't need to run often.
 TODO convert all tests to rust. For now, writing in python was nice and easy,
 but its good practice to write the tests in rust and also probably better practice.
 """
+from http.client import OK
 import requests
 import unittest
+
+from utils.http_response_status_codes import HTTP
 
 ADDRESS = "http://localhost:8000"
 TEST_EMAIL = 'test@gmail.com'
@@ -38,56 +41,62 @@ class ReaderTests(unittest.TestCase):
 
     def test_reader_does_not_exist(self):
         get_reader_not_found = get_reader()
-        self.assertEqual(get_reader_not_found.status_code, 404)
+        self.assertEqual(get_reader_not_found.status_code, HTTP.NOT_FOUND)
 
     def test_insert_then_delete_reader(self):
         add_reader_success = add_reader()
-        self.assertEqual(add_reader_success.status_code, 201)
+        self.assertEqual(add_reader_success.status_code, HTTP.CREATED)
         # User was added successfully
         get_reader_success = get_reader()
-        self.assertEqual(get_reader_success.status_code, 200)
+        self.assertEqual(get_reader_success.status_code, HTTP.OK)
         self.assertEqual(get_reader_success.json(), TEST_USER)
         # Clean up (delete test user)
         delete_reader_success = delete_reader()
-        self.assertEqual(delete_reader_success.status_code, 200)
+        self.assertEqual(delete_reader_success.status_code, HTTP.OK)
         # Check that delete was successful
         get_reader_not_found = get_reader()
-        self.assertEqual(get_reader_not_found.status_code, 404)
+        self.assertEqual(get_reader_not_found.status_code, HTTP.NOT_FOUND)
     
     def test_no_duplicate_users(self):
         add_reader_success = add_reader()
-        self.assertEqual(add_reader_success.status_code, 201)
+        self.assertEqual(add_reader_success.status_code, HTTP.CREATED)
         # Try again
         add_reader_failure = add_reader()
-        self.assertEqual(add_reader_failure.status_code, 403)
+        self.assertEqual(add_reader_failure.status_code, HTTP.FORBIDDEN)
         # Clean up
         delete_reader_success = delete_reader()
-        self.assertEqual(delete_reader_success.status_code, 200)
+        self.assertEqual(delete_reader_success.status_code, HTTP.OK)
 
     def test_update_reader_balance(self):
+        # ensure test user is initially not in DB
+        if get_reader().status_code == OK:
+            delete_reader()
         add_reader_success = add_reader()
-        self.assertEqual(add_reader_success.status_code, 201)
+        self.assertEqual(add_reader_success.status_code, HTTP.CREATED)
         # Update reader
         update_reader_balance_success = update_reader_balance()
-        self.assertEqual(update_reader_balance_success.status_code, 201)
+        self.assertEqual(update_reader_balance_success.status_code, HTTP.CREATED)
         # Verify
         get_reader_success = get_reader()
-        self.assertEqual(get_reader_success.status_code, 200)
+        self.assertEqual(get_reader_success.status_code, HTTP.OK)
         self.assertEqual(get_reader_success.json(), TEST_USER_WITH_MONEY)
         # Take money
         sub_reader_balance_success = sub_reader_balance()
-        self.assertEqual(sub_reader_balance_success.status_code, 201)
+        self.assertEqual(sub_reader_balance_success.status_code, HTTP.CREATED)
         # Verify
         get_reader_success = get_reader()
-        self.assertEqual(get_reader_success.status_code, 200)
+        self.assertEqual(get_reader_success.status_code, HTTP.OK)
         self.assertEqual(get_reader_success.json(), TEST_USER)
         # Clean up
         delete_reader_success = delete_reader()
-        self.assertEqual(delete_reader_success.status_code, 200)
+        self.assertEqual(delete_reader_success.status_code, HTTP.OK)
 
 
 
 if __name__ == '__main__':
+    # Ensure that test user is initially not in DB
+    if get_reader().status_code == HTTP.OK:
+        delete_reader()
     unittest.main()
     # print(add_reader().status_code)
     # delete_reader()
