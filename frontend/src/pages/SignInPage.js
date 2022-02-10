deb5024bba8e41c6db4e806d67a5e3e8cbe877b3
Daniel Wilby <dywilby@gmail.com>
2022-02-10 11:30:28 -0800

    make code neater and document frontend signup/login

--- frontend/src/pages/SignInPage.js
+++ frontend/src/pages/SignInPage.js
@@ -1,112 +1,142 @@
-import React, { useState } from 'react';    
-import { useParams, useNavigate } from 'react-router-dom';    
+import React from 'react';
+import { useParams, useNavigate, Link } from 'react-router-dom';    
 import GoogleLogin from 'react-google-login';    
-import Cookies from 'universal-cookie';
-    
+
 const SignInComponent = ({ subtitle, description, success, failure }) => (    
-        <div className="center-content">    
-    
-                {/* set the width to be the size of 2 columns, and have a breakpoint at the predefined lg */}    
-                <div className="col-lg-2">    
-    
-                        {/* Use the fonts and colors defined in global.css */}    
-                        <h1 className="mb-0 primary-font primary-color fw-bold">PayPerRead</h1>    
-                        <h2 className="primary-font primary-color">{subtitle}</h2>    
-    
-                        <p className="mt-3 secondary-font secondary-color">{description}</p>    
-    
-                        <p className="secondary-font secondary-color">Learn more about how we work <a>here</a>.</p>    
-    
-    
-			{/* TODO: fix styling because the library's implementation of styling is a bitch */}
-                        <GoogleLogin    
-                                clientId="395326925781-gs6ubj69r0egkkeifimohrktr2h3an6p.apps.googleusercontent.com"    
-                                buttonText="Sign in"    
-				icon={false}
-                                onSuccess={success}    
-                                onFailure={failure}    
-                                cookiePolicy={'single_host_origin'}    
-				className="styled-button secondary-font primary-color justify-content-center"
-				style={{ color: "#f00" }}
-                        />    
-                </div>    
-        </div>    
+  <div className="center-content">    
+
+    {/* set the width to be the size of 2 columns, and have a breakpoint at the predefined lg */}    
+    <div className="col-lg-2">    
+
+      {/* Use the fonts and colors defined in global.css */}    
+      <h1 className="mb-0 primary-font primary-color fw-bold">PayPerRead</h1>    
+      <h2 className="primary-font primary-color">{subtitle}</h2>    
+
+      <p className="mt-3 secondary-font secondary-color">{description}</p>    
+
+      {/* TODO: We may put an explanatory page linked here */}
+      <p className="secondary-font secondary-color">Learn more about how we work <Link to="">here</Link>.</p>    
+
+
+      {/* TODO: fix styling because the library's implementation of styling is a bitch */}
+      <GoogleLogin    
+        clientId="395326925781-gs6ubj69r0egkkeifimohrktr2h3an6p.apps.googleusercontent.com"    
+        buttonText="Sign in"    
+        icon={false}
+        onSuccess={success}    
+        onFailure={failure}    
+        cookiePolicy={'single_host_origin'}    
+        className="styled-button secondary-font primary-color justify-content-center"
+        style={{ color: "#f00" }}
+      />    
+    </div>    
+  </div>    
 );    
-    
+
+/*
+ * This function queries our backend to figure out whether the user already
+ * exists in our system, and then calls the corresponding passed in function
+ *
+ * response: the response returned from the google button component
+ * navigate: the react router hook which navigates from a component
+ * userType: 'publisher' or 'reader' based on the slug
+ *
+ * exists: the function to be called if the user exists in our db
+ * doesnt: if they don't
+ */
 const doesUserExist = (response, navigate, userType) => (exists, doesnt) => {
-	console.log(response);
-  	const email = response.profileObj.email;
-	fetch(`http://localhost:8000/${userType}/${email}`).then((resp) => {
-		
-		if(resp.status === 200)
-			exists(response, navigate, userType);
-		else
-			doesnt(response, navigate, userType);
+  const email = response.profileObj.email;
+  fetch(`http://localhost:8000/${userType}/${email}`).then((resp) => {
 
-	})
+    if(resp.status === 200)
+      exists(response, navigate, userType);
+    else
+      doesnt(response, navigate, userType);
 
+  });
 }
 
+
+/*
+ * Tells our frontend to move to a page where the user can verify that they
+ * want to create an account. No account creation happens in this component,
+ * only in VerifySignup.js
+ * 
+ * response: the response returned from the google button component
+ * navigate: the react router hook which navigates from a component
+ * userType: 'publisher' or 'reader' based on the slug
+ */
 const create_new = (response, navigate, userType) => {
-	navigate("/verify-signup", { state: { tokenId: response.tokenId, name: response.profileObj.name, email: response.profileObj.email, userType: userType } });
+  navigate("/verify-signup", { 
+    state: { 
+      tokenId: response.tokenId, 
+      name: response.profileObj.name, 
+      email: response.profileObj.email, 
+      userType: userType 
+    } 
+  });
 
-}
-
-const login = (response, navigate, userType) => {
-	const url = `http://localhost:8000/login/${userType}`;
-        console.log(userType);
-	fetch(url, {
-		method: 'GET',
-                credentials: 'include',
-		headers: new Headers({
-                        'Authorization': `${response.tokenId}`,
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                }
-        )
-	}).then(resp => {
-		if(resp.status === 200) {
-			navigate("/");
-                } else {
-                        console.log(resp.status);
-                }
-	});
 }
 
 /*
-	*/
+ * This function logs into our service with the credentials provided by google.
+ * This function assumes that the user already exists in our system
+ *
+ * response: the response returned from the google button component
+ * navigate: the react router hook which navigates from a component
+ * userType: 'publisher' or 'reader' based on the slug
+ */
+const login = (response, navigate, userType) => {
+
+  const url = `http://localhost:8000/login/${userType}`;
+
+  fetch(url, {
+    method: 'GET',
+    credentials: 'include',
+    headers: new Headers({
+      'Authorization': `${response.tokenId}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    )
+  }).then(resp => {
+    if(resp.status === 200) {
+      navigate("/");
+    } else {
+      console.log(resp.status);
+    }
+  });
+}
 
 const SignInPage = () => {    
-        
-        const navigate = useNavigate();    
-        const { user } = useParams(); // get the slug
-        console.log(user);   
-        // check if the signup is valid
-        if(user !== 'reader' && user !== 'publisher') 
-                return (<div />); // redirect to a 404 page
 
-        const isPublisher = (user === 'publisher');
+  const navigate = useNavigate();
+  const { user } = useParams(); // get the slug
 
-    
-        const success = (response) => {  
-		doesUserExist(response, navigate, user)(login, create_new);
-        };    
-    
-        const failure = (response) => {
-		
-	};
+  // check if the signup is valid
+  if(user !== 'reader' && user !== 'publisher') 
+    return (<div />); // redirect to a 404 page
 
+  const isPublisher = (user === 'publisher');
 
-        return (
-                <SignInComponent 
-                        subtitle={isPublisher ? 'Publisher' : ''} 
-                        description={isPublisher ? 
-                                `Sign up here to add PayPerRead to your site and begin getting paid for your work.` :
-                                `We are a web3-inspired service that helps connect your contribution directly to the author.`
-                        }
-                        success={success}
-                        failure={failure}
-                />
-        );
+  // On the Google signin success
+  const success = (response) => {
+    doesUserExist(response, navigate, user)(login, create_new);
+  };    
+  const failure = (response) => {
+    console.log('Google failure, please try again.');
+  };
+
+  return (
+    <SignInComponent 
+      subtitle={isPublisher ? 'Publisher' : ''} 
+      description={isPublisher ? 
+          `Sign up here to add PayPerRead to your site and begin getting paid for your work.` :
+          `We are a web3-inspired service that helps connect your contribution directly to the author.`
+      }
+      success={success}
+      failure={failure}
+    />
+  );
 };
 
 export default SignInPage;
--- frontend/src/pages/VerifySignup.js
+++ frontend/src/pages/VerifySignup.js
@@ -1,60 +1,60 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+/*
+ * Create the user and then redirect to the homepage
+ */
 const create_new_user = (details, navigate) => () => {
 
-	const payload = {
-		email: details.state.email,
-		name: details.state.name
-	};
-	const isPublisher = details.state.userType == 'publisher';
-	if (isPublisher) {
-		payload.domain = "abc.com";
-	}
-	const p = JSON.stringify(payload);
-	console.log(p);
+  const isPublisher = details.state.userType === 'publisher';
 
-	// TODO: publisher endpoint yet to be implemeneted
-	const url = isPublisher ? 'http://localhost:8000/publisher/new-publisher' : 'http://localhost:8000/reader/new-reader';
+  const payload = {
+    email: details.state.email,
+    name: details.state.name
+  };
+  if (isPublisher)
+    payload.domain = "abc.com";
 
-	// A post with no-cors has less options for content-type
-	fetch(url, {
-		method: 'POST',
-		// credentials:'same-origin',
-		credentials: 'include',
-		redirect: 'follow',
-		referrerPolicy: 'no-referrer',
-		headers: {
-			'Authorization': details.state.tokenId,
-			'Content-type': 'application/json',
+  const p = JSON.stringify(payload);
 
-		},
-		body: JSON.stringify(payload)
-	}).then(resp => {
-		console.log(resp);
-		if(resp.status === 201)
-			navigate("/");
-	});
+  const url = isPublisher ? 'http://localhost:8000/publisher/new-publisher' : 'http://localhost:8000/reader/new-reader';
+
+  // A post with no-cors has less options for content-type
+  fetch(url, {
+    method: 'POST',
+    // credentials:'same-origin',
+    credentials: 'include',
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    headers: {
+      'Authorization': details.state.tokenId,
+      'Content-type': 'application/json',
+
+    },
+    body: p
+  }).then(resp => {
+    if(resp.status === 201)
+      navigate("/");
+  });
 
 };
 
 const VerifySignup = () => {
 
-	const navigate = useNavigate();
-	const location = useLocation();
-	console.log(location)
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	return (
-		<div className="center-content">
-			<div className="col-lg-2">
-			<h1>Verify</h1>
-			<p>Hi {location.state.name},</p>
-			<p>Click verify to create your account with the following email,</p>
-			<p>{location.state.email}</p>
-			<button className="styled-button" onClick={create_new_user(location, navigate)}>Verify Signup</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="center-content">
+      <div className="col-lg-2">
+        <h1>Verify</h1>
+        <p>Hi {location.state.name},</p>
+        <p>Click verify to create your account with the following email,</p>
+        <p>{location.state.email}</p>
+        <button className="styled-button" onClick={create_new_user(location, navigate)}>Verify Signup</button>
+      </div>
+    </div>
+  );
 }
 
 
