8751f4c76ced87feb331597eee5a4fe411b86aad
Xin <xin2001yan@gmail.com>
2022-02-10 15:17:56 -0800

    update release plan and upload sprint 2 report

--- backend/tests/publishers.py
+++ backend/tests/publishers.py
@@ -2,6 +2,7 @@ import json
 from pickle import ADDITEMS
 import requests
 import unittest
+from utils.http_response_status_codes import HTTP
 
 ADDRESS = "http://localhost:8000"
 TEST_EMAIL = "test@gmail.com"
@@ -12,6 +13,7 @@ TEST_USER_NAME = 'Test User'
 NEW_USER = {'email': TEST_EMAIL, 'name': TEST_USER_NAME, 'domain': TEST_DOMAIN}
 TEST_USER = {'email': TEST_EMAIL, 'name': TEST_USER_NAME, 'domain': TEST_DOMAIN}
 TEST_USER_WITH_MONEY = {'email': TEST_EMAIL, 'name': TEST_USER_NAME, 'balance': MONEY}
+        
 
 def get_publishers():
     return requests.get(f"{ADDRESS}/publishers")
@@ -22,19 +24,27 @@ def add_publisher():
 def get_publisher():
     return requests.get(f"{ADDRESS}/publisher/{TEST_EMAIL}")
 
+def delete_publisher():
+    return requests.delete("{ADDRESS}/publisher/{TEST_EMAIL}")
+
+def set_initial_state():
+    get_publisher_result = get_publisher()
+    if get_publisher_result.status_code == HTTP.OK:
+        delete_publisher()
 
 class PublisherTests(unittest.TestCase):
 
     def test_publisher_does_not_exist(self):
-        get_publisher_not_found = get_publisher()
-        self.assertEqual(get_publisher_not_found.status_code, 404)
+        get_publisher_result = get_publisher()
+        self.assertEqual(get_publisher_result.status_code, HTTP.NOT_FOUND)
 
     def test_insert_then_delete_publisher(self):
         add_publisher_result = add_publisher()
-        self.assertEqual(add_publisher_result.status_code, 201)
+        self.assertEqual(add_publisher_result.status_code, HTTP.CREATED)
         get_publisher_result = get_publisher()
-        self.assertEqual(get_publisher_result.status_code, 200)
+        self.assertEqual(get_publisher_result.status_code, HTTP.OK)
 
 
 if __name__ == "__main__":
+    set_initial_state()
     unittest.main()
